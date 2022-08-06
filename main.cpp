e61410337613ecb3753ca87f7a2012b7e5323cf2
Soumya Lodh <81670203+slodh200634@users.noreply.github.com>
2022-08-07 00:48:32 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,14 @@
 #include<iostream>
 #include<cstdio>
 using namespace std;
-
-
-
 int main() {
     int nn;
     cin>>nn;
     int const n = nn;
     int const n2 = n*n;
-
     int c,x,y,z,xy;
     c=n+1;
     x=1;
-
-
 while(true){
     if (x<n2)
     {
